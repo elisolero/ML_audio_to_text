5f246702ce5f96ef1deae8cedd289e4402d2715c
Your Name <you@example.com>
2018-06-14 15:20:07 +0300

    add templates render data

---
+++ sentiment.py
@@ -0,0 +1,49 @@
+from textblob import TextBlob
+import json
+
+def Sentiments():
+
+    with open('chatbot_data.json') as f:
+        data = json.load(f)
+
+    mainList = []
+    negativeList = []
+    positiveList = []
+    negativeCount = 0;
+    positiveCount = 0;
+
+    for val in data:
+        json_string = val['jsonValue']
+        obj = json.loads(json_string)
+        text = obj['userInput'].strip()
+        if(text != 'hello' and text != ''):
+            mainList.append(text)
+
+    for userInput in mainList:
+        # print(userInput)
+        analysis = TextBlob(userInput)
+        # print(analysis.sentiment)
+        if(analysis.sentiment.polarity <= -0.9):
+            negativeCount += 1
+            negativeList.append(userInput)
+        elif(analysis.sentiment.polarity >= 0.9):
+            positiveCount += 1
+            positiveList.append(userInput)
+            # print(userInput)
+            # print(analysis.sentiment)
+
+    # print(positiveCount)
+
+    dict = {
+        'positiveCount': positiveCount,
+        'negativeCount': negativeCount,
+        'positiveList': positiveList,
+        'negativeList': negativeList,
+    }
+
+    # print(*positiveList, sep='\n')
+    #
+    # print(negativeCount)
+    # print(*negativeList, sep='\n')
+
+    return dict;
