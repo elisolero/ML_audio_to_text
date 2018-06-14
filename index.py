0d435b6f3cddda11d1937879f4944b6bf3aca145
Your Name <you@example.com>
2018-06-14 13:33:53 +0300

    add Flask for web service

--- index.py
+++ index.py
@@ -1,38 +1,50 @@
-from textblob import TextBlob
-import json
+from flask import Flask
+app = Flask(__name__)
 
-with open('chatbot_data.json') as f:
-    data = json.load(f)
+@app.route("/")
+def hello():
+    return "Hello World!!"
 
-mainList = []
-negativeList = []
-positiveList = []
-negativeCount = 0;
-positiveCount = 0;
 
-for val in data:
-    json_string = val['jsonValue']
-    obj = json.loads(json_string)
-    text = obj['userInput'].strip()
-    if(text != 'hello' and text != ''):
-        mainList.append(text)
 
-for userInput in mainList:
-    # print(userInput)
-    analysis = TextBlob(userInput)
-    # print(analysis.sentiment)
-    if(analysis.sentiment.polarity <= -0.9):
-        negativeCount += 1
-        negativeList.append(userInput)
-    elif(analysis.sentiment.polarity >= 0.9):
-        positiveCount += 1
-        positiveList.append(userInput)
+
+# from textblob import TextBlob
+# import json
+#
+# with open('chatbot_data.json') as f:
+#     data = json.load(f)
+#
+# mainList = []
+# negativeList = []
+# positiveList = []
+# negativeCount = 0;
+# positiveCount = 0;
+#
+# for val in data:
+#     json_string = val['jsonValue']
+#     obj = json.loads(json_string)
+#     text = obj['userInput'].strip()
+#     if(text != 'hello' and text != ''):
+#         mainList.append(text)
+#
+# for userInput in mainList:
+#     # print(userInput)
+#     analysis = TextBlob(userInput)
+#     # print(analysis.sentiment)
+#     if(analysis.sentiment.polarity <= -0.9):
+#         negativeCount += 1
+#         negativeList.append(userInput)
+#     elif(analysis.sentiment.polarity >= 0.9):
+#         positiveCount += 1
+#         positiveList.append(userInput)
         # print(userInput)
         # print(analysis.sentiment)
 
-print(positiveCount)
-print(*positiveList, sep='\n')
+# print(positiveCount)
+# print(*positiveList, sep='\n')
+#
+# print(negativeCount)
+# print(*negativeList, sep='\n')
 
-print(negativeCount)
-print(*negativeList, sep='\n')
-
+if __name__ == '__main__':
+    app.run(debug=True)
